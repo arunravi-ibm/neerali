c6b8e428b0895b3c553d6f061eeee078157f84ac
Arunkumar Ravindranath <arunkumar.ravindranath@ibm.com>
2024-10-22 11:35:11 +0000

    Changed Fio Build setting not to copy yum.repos.d, Mount instead Added new options to runfio.sh script  - --rw, --bs and --iodepth

--- roles/fio/files/runfio.sh
+++ roles/fio/files/runfio.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-LONGOPTS=block-device:,time-to-run:,num-jobs:,dev-type,file-size
+LONGOPTS=block-device:,time-to-run:,num-jobs:,dev-type:,file-size:,iodepth:
 OPTIONS=b:t:n:d:s
 
 devtype=fs
 filesize="1G"
 numjobs=1
 timetorun=0
+iodepth=128
+bs=4k
+rw=randwrite
 device=""
 
 _args=$(getopt --options=${OPTIONS} --longoptions=${LONGOPTS} --name "$@" -- "$@")
@@ -21,23 +24,35 @@ while [ "$#" -gt 0 ]; do
       timetorun=$2
       shift 2
       ;;
-	-n|--num-jobs)
+        -n|--num-jobs)
       numjobs=$2
       shift 2
       ;;
-	-d|--dev-type)
+      -d|--dev-type)
       devtype=$2
       shift 2
       ;;
-	-s|--file-size)
-	  filesize=$2
-	  shift 2
-	  ;;
-    --)
-	  shift 2
+      -s|--file-size)
+        filesize=$2
+        shift 2
+        ;;
+      --iodepth)
+        iodepth=$2
+        shift 2
+        ;;
+      --bs)
+        bs=$2
+        shift 2
+        ;;
+      --rw)
+        rw=$2
+        shift 2
+          ;;
+      --)
+          shift 2
       break
       ;;
-	*)
+        *)
       echo "Unknown option"
       exit 3
   esac
@@ -48,15 +63,15 @@ FIO_CMD="fio"
 FIO_ARGS="--group_reporting --direct=1 --ioengine=libaio"
 
 if [[ $timetorun -gt 0 ]]; then
-	FIO_ARGS="$FIO_ARGS --time_based --runtime=$timetorun"
+        FIO_ARGS="$FIO_ARGS --time_based --runtime=$timetorun"
 fi
 if [[ ${devtype} == "blk" ]]; then
-	FIO_ARGS="$FIO_ARGS --name=seq_write_test --filename=$device --rw=write --bs=1M --numjobs=$numjobs"
+        FIO_ARGS="$FIO_ARGS --name=neerali_${devtype}_test --filename=$device --rw=$rw --bs=$bs --numjobs=$numjobs --iodepth=$iodepth"
 elif [[ ${devtype} == "fs" ]]; then
-	FIO_ARGS="$FIO_ARGS --name=nfs_test --directory=/data --rw=randwrite --bs=4k --size=$filesize --numjobs=$numjobs"
-else 
-	echo "Invalid Arguments"
-	exit 1
+        FIO_ARGS="$FIO_ARGS --name==neerali_${devtype}_test --directory=/data --rw=$rw --bs=$bs --size=$filesize --numjobs=$numjobs --iodepth=$iodepth"
+else
+        echo "Invalid Arguments"
+        exit 1
 fi
 
 echo Running: $FIO_CMD $FIO_ARGS
